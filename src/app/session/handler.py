7133f38a72d281a626f2a2d8a9a9b88e6f2eed95
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2020-01-14 17:35:30 +0100

    small code refractoring
    test endpoints

--- src/app/session/handler.py
+++ src/app/session/handler.py
@@ -6,15 +6,13 @@ session_handler = Blueprint('session_handler', __name__, template_folder='templa
 
 LOG = logging.getLogger(__name__)
 
-@session_handler.route('/login')
-def login_page():
-    return render_template('login.html')
 
-@session_handler.route('/get/')
+@session_handler.route('/test/session/get/')
 def get_key():
     return session.get('key', 'not set')
 
-@session_handler.route('/set/')
+
+@session_handler.route('/test/session/set/')
 def set_key():
     session.setdefault('key', default='value is set')
     return 'ok'
