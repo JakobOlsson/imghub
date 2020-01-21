aea07607742352c3f30b28a58b3cf8592334cb7b
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2020-01-21 23:02:52 +0100

    moved bucket settings variables
    moved bucket settings variable to common settings file. little uggly fix to get variables in to blueprints that dont gets global variables on load

--- src/app/bucket/handler.py
+++ src/app/bucket/handler.py
@@ -1,45 +1,38 @@
 import boto3
 import logging
-from os import getenv
 from flask import Blueprint, render_template
 from flask import request, redirect, jsonify
 from werkzeug.utils import secure_filename
-bucket_handler = Blueprint('bucket_handler', __name__, template_folder='templates')
+from settings import S3_API_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from settings import S3_PREFIX, S3_BUCKET_NAME, S3_FULL_URL
+
+
+bucket_handler = Blueprint('bucket_handler',
+                           __name__,
+                           template_folder='templates')
 
 LOG = logging.getLogger(__name__)
 
-AWS_ACCESS_KEY_ID=getenv('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY=getenv('AWS_SECRET_ACCESS_KEY', '')
-# for aws this is https://s3.<region>.amazonaws.com/
-# or https://s3.amazonaws.com/
-s3_api_endpoint = getenv('S3_API_ENDPOINT', 'http://localhost:9000')
-
 s3 = boto3.resource('s3',
-                    endpoint_url=s3_api_endpoint,
+                    endpoint_url=S3_API_ENDPOINT,
                     config=boto3.session.Config(signature_version='s3v4'),
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 if AWS_ACCESS_KEY_ID == '':
     s3 = boto3.resource('s3',
-                        endpoint_url=s3_api_endpoint,
+                        endpoint_url=S3_API_ENDPOINT,
                         config=boto3.session.Config(signature_version='s3v4'))
-'''
-s3cl = boto3.client('s3',
-                    aws_access_key_id=getenv('AWS_ACCESS_KEY_ID', "AKIAIOSFODNN7EXAMPLE"),
-                    aws_secret_access_key=getenv('AWS_SECRET_ACCESS_KEY', "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"))
-'''
-s3_bucket_name = getenv('S3_BUCKET', 'images')
-s3_prefix = getenv("S3_PREFIX", 'images')
-s3_full_url = f'{s3_api_endpoint}/{s3_bucket_name}' if getenv('S3_FULL_URL', "") == "" else getenv('S3_FULL_URL')
 
 
 @bucket_handler.route('/upload')
 def upload_file_page():
     return render_template('upload.html')
 
+
 @bucket_handler.route('/listimages')
 def listimages_page():
     return render_template('list.html')
+
 
 @bucket_handler.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
@@ -52,12 +45,12 @@ def upload_file():
             return redirect('/upload')
         if rec_file:
             filename = secure_filename(rec_file.filename)
-            bucket = s3.Bucket(s3_bucket_name)
+            bucket = s3.Bucket(S3_BUCKET_NAME)
             filetype = filename.split('.')[-1]
             if 3 > len(filetype) < 4:
                 filetype = "jpeg"
             ContentType = 'image/{}'.format(filetype)
-            Key = f"{s3_prefix}/{filename}"
+            Key = f"{S3_PREFIX}/{filename}"
             LOG.info(f"uploading: {filename}")
             bucket.put_object(Body=rec_file,
                               Key=Key,
@@ -67,12 +60,12 @@ def upload_file():
 
 @bucket_handler.route('/list')
 def list_imgs():
-    bucket = s3.Bucket(s3_bucket_name)
-    objs = bucket.objects.filter(Prefix=s3_prefix)
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    objs = bucket.objects.filter(Prefix=S3_PREFIX)
     ret = {"files": []}
     for x in objs:
         filename = x.key
-        file_path = "{}/{}".format(s3_full_url, filename)
+        file_path = "{}/{}".format(S3_FULL_URL, filename)
         file_inf = {'url': file_path, 'name': filename}
         ret['files'].append(file_inf)
     return jsonify(ret)
@@ -82,7 +75,7 @@ def list_imgs():
 def delete_img(image):
     LOG.info(f"delete: {image}")
 
-    bucket = s3.Bucket(s3_bucket_name)
+    bucket = s3.Bucket(S3_BUCKET_NAME)
     bucket.delete_objects(
         Delete={
             'Objects': [
@@ -97,9 +90,9 @@ def delete_img(image):
 
 @bucket_handler.route('/test/bucket/access')
 def test_bucket():
-    bucket = s3.Bucket(s3_bucket_name)
+    bucket = s3.Bucket(S3_BUCKET_NAME)
     try:
-        bucket.objects.filter(Prefix=s3_prefix)
+        bucket.objects.filter(Prefix=S3_PREFIX)
         return "ok"
     except:
         return "error"
--- src/app/settings.py
+++ src/app/settings.py
@@ -4,3 +4,19 @@ import redis
 SESSION_KEY = getenv('SESSION_KEY')
 SESSION_TYPE = getenv('SESSION_TYPE')
 SESSION_REDIS = redis.from_url(getenv('SESSION_REDIS'))
+
+# S3BUCKET
+AWS_ACCESS_KEY_ID = getenv('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = getenv('AWS_SECRET_ACCESS_KEY', '')
+# for aws this is https://s3.<region>.amazonaws.com/
+# or https://s3.amazonaws.com/
+S3_API_ENDPOINT = getenv('S3_API_ENDPOINT', 'http://localhost:9000')
+
+# Name of the bucket
+S3_BUCKET_NAME = getenv('S3_BUCKET', 'images')
+# Prefix (folder) in the Bucket
+S3_PREFIX = getenv("S3_PREFIX", 'images')
+# Full url for the bucket, custom dns name if for instance,
+# fronted by CloudFront.
+# Remember to include https or http if it is secure or not
+S3_FULL_URL = f'{S3_API_ENDPOINT}/{S3_BUCKET_NAME}' if getenv('S3_FULL_URL', "") == "" else getenv('S3_FULL_URL')
